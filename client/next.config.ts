64e6a9c8bf160483761cf747fa2fc08b5112a870
dipan-ck <dipan2003.dev@gmail.com>
2026-03-06 19:23:48 +0000

    fixed ui and improved mediasoup configs

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
+  reactCompiler: true,
 
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
